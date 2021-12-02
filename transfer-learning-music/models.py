c1a215d331db582a0a46ce136e6851d5220386de
Pablo <pablo.alonso@upf.edu>
2021-12-02 19:02:48 +0100

    Remove unused output

---
+++ transfer-learning-music/models.py
@@ -0,0 +1,131 @@
+models = [
+    {
+        "name": "genre_dortmund",
+        "labels": [
+            "alternative",
+            "blues",
+            "electronic",
+            "folkcountry",
+            "funksoulrnb",
+            "jazz",
+            "pop",
+            "raphiphop",
+            "rock",
+        ],
+    },
+    {
+        "name": "genre_rosamerica",
+        "labels": [
+            "classic",
+            "dance",
+            "hip hop",
+            "jazz",
+            "pop",
+            "rhythm and blues",
+            "rock",
+            "speech",
+        ],
+    },
+    {
+        "name": "genre_tzanetakis",
+        "labels": [
+            "blues",
+            "classic",
+            "country",
+            "disco",
+            "hip hop",
+            "jazz",
+            "metal",
+            "pop",
+            "reggae",
+            "rock",
+        ],
+    },
+    {
+        "name": "genre_electronic",
+        "labels": [
+            "ambient",
+            "dnb",
+            "house",
+            "techno",
+            "trance",
+        ],
+    },
+    {
+        "name": "mood_acoustic",
+        "labels": [
+            "acoustic",
+            "not acoustic",
+        ],
+    },
+    {
+        "name": "mood_electronic",
+        "labels": [
+            "electronic",
+            "not electronic",
+        ],
+    },
+    {
+        "name": "mood_aggressive",
+        "labels": [
+            "aggressive",
+            "not aggressive",
+        ],
+    },
+    {
+        "name": "mood_relaxed",
+        "labels": [
+            "not relaxed",
+            "relaxed",
+        ],
+    },
+    {
+        "name": "mood_happy",
+        "labels": [
+            "happy",
+            "not happy",
+        ],
+    },
+    {
+        "name": "mood_sad",
+        "labels": [
+            "not sad",
+            "sad",
+        ],
+    },
+    {
+        "name": "mood_party",
+        "labels": [
+            "not party",
+            "party",
+        ],
+    },
+    {
+        "name": "danceability",
+        "labels": [
+            "danceable",
+            "not danceable",
+        ],
+    },
+    {
+        "name": "gender",
+        "labels": [
+            "female",
+            "male",
+        ],
+    },
+    {
+        "name": "tonal_atonal",
+        "labels": [
+            "tonal",
+            "atonal",
+        ],
+    },
+    {
+        "name": "voice_instrumental",
+        "labels": [
+            "instrumental",
+            "voice",
+        ],
+    },
+]
--- transfer-learning-music/predict.py
+++ transfer-learning-music/predict.py
@@ -20,69 +20,9 @@ from essentia.streaming import (
 from essentia import Pool, run
 import numpy as np
 
+from models import models
 
 MODELS_HOME = "/models"
-
-# TODO Factor out the list of models to a separate file.
-models = [
-    {
-        "name": "genre_dortmund",
-        "labels": [
-            "alternative",
-            "blues",
-            "electronic",
-            "folkcountry",
-            "funksoulrnb",
-            "jazz",
-            "pop",
-            "raphiphop",
-            "rock",
-        ],
-    },
-    {
-        "name": "genre_rosamerica",
-        "labels": [
-            "classic",
-            "dance",
-            "hip hop",
-            "jazz",
-            "pop",
-            "rhythm and blues",
-            "rock",
-            "speech",
-        ],
-    },
-    {
-        "name": "genre_tzanetakis",
-        "labels": [
-            "blues",
-            "classic",
-            "country",
-            "disco",
-            "hip hop",
-            "jazz",
-            "metal",
-            "pop",
-            "reggae",
-            "rock",
-        ],
-    },
-    {
-        "name": "genre_electronic",
-        "labels": ["ambient", "dnb", "house", "techno", "trance"],
-    },
-    {"name": "mood_acoustic", "labels": ["acoustic", "not acoustic"]},
-    {"name": "mood_electronic", "labels": ["electronic", "not electronic"]},
-    {"name": "mood_aggressive", "labels": ["aggressive", "not aggressive"]},
-    {"name": "mood_relaxed", "labels": ["not relaxed", "relaxed"]},
-    {"name": "mood_happy", "labels": ["happy", "not happy"]},
-    {"name": "mood_sad", "labels": ["not sad", "sad"]},
-    {"name": "mood_party", "labels": ["not party", "party"]},
-    {"name": "danceability", "labels": ["danceable", "not danceable"]},
-    {"name": "gender", "labels": ["female", "male"]},
-    {"name": "tonal_atonal", "labels": ["tonal", "atonal"]},
-    {"name": "voice_instrumental", "labels": ["instrumental", "voice"]},
-]
 
 
 class Predictor(cog.Predictor):
@@ -109,14 +49,7 @@ class Predictor(cog.Predictor):
         options=["musicnn-msd-2", "musicnn-mtt-2", "vggish-audioset-1"],
         help="Model type (embeddings)",
     )
-    @cog.input(
-        "output_format",
-        type=str,
-        default="Visualization",
-        options=["Visualization", "JSON"],
-        help="Output either a bar chart visualization or a JSON blob",
-    )
-    def predict(self, audio, url, model_type, output_format):
+    def predict(self, audio, url, model_type):
         """Run a single prediction by all models of the selected type"""
 
         assert audio or url, "A filename or a YouTube URL should be specified"
