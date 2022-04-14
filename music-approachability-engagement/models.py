d5f81862f4f171e0195bff4e42549592c5fa18b1
xaviliz <xavilizarraga@gmail.com>
2022-04-14 16:20:15 +0200

    Redefine models by model_type.

--- music-approachability-engagement/models.py
+++ music-approachability-engagement/models.py
@@ -1,17 +1,51 @@
-# define the model labels for approachability and engagement
-models = [
-    {
-        "name": "approachability",
-        "labels": [
-            "not approachable",
-            "approachable",
-        ],
+# define the model labels for approachability and engagement per model_type
+models = {
+    "effnet-discogs-test-2class":{
+        "approachability": {
+            "name": "approachability-2class",
+            "labels": [
+                "not approachable",
+                "approachable",
+            ],
+        },
+        "engagement": {
+            "name": "engagement-2class",
+            "labels": [
+                "not engaging",
+                "engaging",
+            ],
+        },
     },
-    {
-        "name": "engagement",
-        "labels": [
-            "not engaging",
-            "engaging",
-        ],
+    "effnet-discogs-test-3class":{
+        "approachability": {
+            "name": "approachability-3class",
+            "labels": [
+                "not approachable",
+                "approachable",
+            ],
+        },
+        "engagement": {
+            "name": "engagement-3class",
+            "labels": [
+                "not engaging",
+                "engaging",
+            ],
+        },
     },
-]
+    "effnet-discogs-test-regression":{
+        "approachability": {
+            "name": "approachability-regression",
+            "labels": [
+                "mean",
+                "std",
+            ],
+        },
+        "engagement": {
+            "name": "engagement-regression",
+            "labels": [
+                "mean",
+                "std",
+            ],
+        },
+    }
+}
